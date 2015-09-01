ca86ced52f5afa94c1975873b64482de78a508c3
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-09-01 12:52:15 -0500

    added post method

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var app = express();
 
 
 app.listen(process.env.PORT || 5000);
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.post('/slackintegration', function (req, res) {
+  res.send(req.body);
 });
-
-app.use(express.static('dist'));
-app.use(express.static('app'));
