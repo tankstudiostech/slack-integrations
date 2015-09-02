d68527ef1a8ffcec0f60a5951dbf77ff2633bd6b
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-09-02 11:49:15 -0500

    got auto invite to techreformation working

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() ); 
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
-
+var techrefemail = 'techreformation.slack.com';
+var techreftoken = 'xoxp-6901139172-6902652471-10022340690-436360'
 app.listen(process.env.PORT || 5000);
-app.post('/slackintegration', function (req, res) {
-  res.send("rawr");
+app.post('/techrefinvite', function (req, res) {
+  if(req.body.email != undefined)
+  {
+    InviteToSlack(techrefemail, req.body.email, techreftoken);
+    res.send("success");  
+  }
+  else
+  {
+  res.send("Failed");
+  }
 });
+
+function InviteToSlack(url, email, token) {
+
+  var options = {
+    proxy: process.env.https_proxy,
+    url:   'https://' + url + '/api/users.admin.invite?email=' + email + '&channels=C06SGVBV5&first_name=Ryan&last_name=Tankersley&token=' + token + '&set_active=true&_attempts=1',
+      method: 'POST',
+  };
+  var req = request.post(options, function(err, res, body) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  console.log('BODY: ' + body);
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+}
