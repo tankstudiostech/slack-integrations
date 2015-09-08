07f4fa599f2fafa1e570c3d01a7e6d364d770893
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-09-08 12:22:40 -0500

    erased invite.  Just want to work

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ app.listen(process.env.PORT || 5000);
 
 app.post('/techrefinvite', function (req, res) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-      InviteToSlack(techrefurl, req.body.email, req.body.fname, req.body.lname, techreftoken, function(data) {
-        res.setEncoding('utf8');
-        res.send(data);
-      });
+    res.send("{\"ok\":false}")
+      // InviteToSlack(techrefurl, req.body.email, req.body.fname, req.body.lname, techreftoken, function(data) {
+      //   res.setEncoding('utf8');
+      //   res.send(data);
+      // });
 });
 
 function InviteToSlack(url, email, fname, lname, token, callback) {
