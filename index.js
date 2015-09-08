66808188398d5e1f5f511a8c4da735afb2481b65
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-09-08 12:29:14 -0500

    Fixed...I think

--- index.js
+++ index.js
@@ -13,26 +13,22 @@ var techreftoken = 'xoxp-6901139172-6902652471-10022340690-436360'
 app.listen(process.env.PORT || 5000);
 
 app.post('/techrefinvite', function (req, res) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    var callback = function(data) {
-      res.setEncoding('utf8');
-      res.send(data);
-    };
-    callback("{\"ok\":false}");
-      // InviteToSlack(techrefurl, req.body.email, req.body.fname, req.body.lname, techreftoken, function(data) {
-      //   res.setEncoding('utf8');
-      //   res.send(data);
-      // });
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+  InviteToSlack(techrefurl, req.body.email, req.body.fname, req.body.lname, techreftoken, res, function (data, newRes) {
+    newRes.setEncoding('utf8');
+    newRes.send(data);
+  });
 });
 
-function InviteToSlack(url, email, fname, lname, token, callback) {
-    var options = {
-      proxy: process.env.https_proxy,
-      url: 'https://' + url + '/api/users.admin.invite?email=' + email + '&channels=C06SGVBV5&first_name=' + fname + 'n&last_name=' + lname + '&token=' + token + '&set_active=true&_attempts=1',
-      method: 'POST',
-    };
-    
-    request.post(options, function (err, res, body) {
-      callback(body);
-    });
+function InviteToSlack(url, email, fname, lname, token, originalRes, callback) {
+  var options = {
+    proxy: process.env.https_proxy,
+    url: 'https://' + url + '/api/users.admin.invite?email=' + email + '&channels=C06SGVBV5&first_name=' + fname + 'n&last_name=' + lname + '&token=' + token + '&set_active=true&_attempts=1',
+    method: 'POST',
+  };
+
+  request.post(options, function (err, res, body) {
+    callback(body, originalRes);
+  });
 }
