bc714b8d60642b91a8e76cb0b282575f242140b4
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-09-08 12:15:41 -0500

    Moved header

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ var techrefemail = 'techreformation.slack.com';
 var techreftoken = 'xoxp-6901139172-6902652471-10022340690-436360'
 app.listen(process.env.PORT || 5000);
 app.post('/techrefinvite', function (req, res) {
-  try {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    InviteToSlack(techrefemail, req.body.email, req.body.fname, req.body.lname, techreftoken, function(data) {
-      res.setEncoding('utf8');
-      res.send(data);
-    });
-  } catch(err) {
-      res.setEncoding('utf8');
-      res.send("{\"ok\":false}");
-  }
+    try {
+      InviteToSlack(techrefemail, req.body.email, req.body.fname, req.body.lname, techreftoken, function(data) {
+        res.setEncoding('utf8');
+        res.send(data);
+      });
+    } catch(err) {
+        res.setEncoding('utf8');
+        res.send("{\"ok\":false}");
+    }
 });
 
 function InviteToSlack(url, email, fname, lname, token, callback) {
