c0c34fcef6964a9f22d455ddfeac51321cdf80eb
Ryan Tankersley <ryan.tankersley@interworks.com>
2016-01-26 08:30:26 -0600

    Added an on enter command for the slack. Hopefully it works.

--- scripts/callpaul.js
+++ scripts/callpaul.js
@@ -2,4 +2,10 @@ module.exports = function(robot) {
     robot.hear(/call paul/i, function(msg) {
         msg.send('http://i1068.photobucket.com/albums/u458/Ryan_Tankersley/apostlepaul_zpsgdj6cijj.jpg');  
     });
+    
+    robot.enter(function(res) {
+        if(res.envelope.room == 'general') {
+            res.send('Welcome!');
+        }
+    });
 }
