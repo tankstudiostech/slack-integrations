908f840b2f441df7fbb6b422750539f737c706e6
Ryan Tankersley <ryan.tankersley@interworks.com>
2016-05-30 13:10:34 -0500

    Removed test message from call paul

--- scripts/callpaul.js
+++ scripts/callpaul.js
@@ -1,6 +1,5 @@
 module.exports = function(robot) {
     robot.hear(/call paul/i, function(msg) {
-        msg.send('rawr');
         msg.send('http://i1068.photobucket.com/albums/u458/Ryan_Tankersley/apostlepaul_zpsgdj6cijj.jpg');  
     });
     
