71c9bc51e5184beef87a6a6d634548ad0e654d95
Ryan Tankersley <ryan.tankersley@interworks.com>
2016-05-30 13:01:15 -0500

    Test code

--- scripts/callpaul.js
+++ scripts/callpaul.js
@@ -1,5 +1,6 @@
 module.exports = function(robot) {
     robot.hear(/call paul/i, function(msg) {
+        msg.send('rawr');
         msg.send('http://i1068.photobucket.com/albums/u458/Ryan_Tankersley/apostlepaul_zpsgdj6cijj.jpg');  
     });
     
